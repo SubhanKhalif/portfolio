e383846908b7119fd0dd3041f74379d0c31c7325
SubhanKhalif <subhankhalif34@gmail.com>
2024-12-27 22:37:53 +0530

    updated

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ window.onscroll = () => {
     document.querySelector('#menu').classList.remove('fa-times');
     document.querySelector('.navbar').classList.remove('nav-toggle');
 }
+
+
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["frontend development", "web designing", "web development"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
+// <!-- typed js effect ends -->
